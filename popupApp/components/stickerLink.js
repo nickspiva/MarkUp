ff5bedf6e39f2de7d29b60efa0d2dd567aa46273
Nicky Spiva <nick.spiva@gmail.com>
2020-06-04 22:43:37 -0500

    open new tab with url of sticker

--- popupApp/components/stickerLink.js
+++ popupApp/components/stickerLink.js
@@ -5,17 +5,26 @@ const stickerLink = (props) => {
   const { message, url, createdAt } = sticker;
   let dateCreated = new Date(Date.parse(createdAt));
 
+  const openNewTab = (newUrl) => {
+    chrome.tabs.create({ url: newUrl });
+  };
+
   return (
     <div
       style={{
         border: "solid black",
+        background: "lightblue",
         backgroudColor: "lightblue",
         margin: "10px",
       }}
+      onClick={() => openNewTab(url)}
     >
       <div>Message: {message} </div>
       <div>URL: {url}</div>
-      <div>Created: {dateCreated.getDate()}</div>
+      <div>
+        Created: {dateCreated.getMonth()}/{dateCreated.getDate()} @{" "}
+        {dateCreated.getTime()}
+      </div>
     </div>
   );
 };
