223d66ed07385672181e630a9bde4d908d1e2f72
Nicky Spiva <nick.spiva@gmail.com>
2020-06-22 18:49:40 -0400

    updating db with heroku url

--- popupApp/components/ngrok.js
+++ popupApp/components/ngrok.js
@@ -1,3 +1,4 @@
-const ngrokUrl = "http://2c42fd1e889b.ngrok.io/";
+//const ngrokUrl = "http://2c42fd1e889b.ngrok.io/";
+const ngrokUrl = "https://markup-extension.herokuapp.com/";
 
 module.exports = ngrokUrl;
