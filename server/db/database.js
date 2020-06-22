0e4760e42bec5af626bb95f2a63c0dcbc8067b47
Nicky Spiva <nick.spiva@gmail.com>
2020-06-22 18:15:56 -0400

    updating heroku database_URL for sequelize connection

--- server/db/database.js
+++ server/db/database.js
@@ -16,8 +16,11 @@ const pkg = require("../../package.json");
 const dbName = `${pkg.name}-test`;
 console.log(`Opening database connection to ${dbName}`);
 
-const db = new Sequelize(`postgres://localhost:5432/${dbName}`, {
-  logging: false,
-});
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
+  {
+    logging: false,
+  }
+);
 
 module.exports = db;
