3e3067d74523d85fcd2083e33d6cc1f079a09fb7
Nicky Spiva <nick.spiva@gmail.com>
2020-06-30 16:29:25 -0400

    removing files from git tracking

--- secrets.js
+++
@@ -1,2 +0,0 @@
-//need to add google client ID, secret, callback at somepoint
-process.env.DATABASE_URL = `postgres://lqngssbkyaomwu:01a9c50592f1fd50e67cd5c889bd55c8ae61e82e523f21839d81b201574bb0c7@ec2-35-173-94-156.compute-1.amazonaws.com:5432/dd0258seii6fcv`;
